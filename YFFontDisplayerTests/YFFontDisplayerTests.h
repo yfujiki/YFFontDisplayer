81b13130699a024b3baba91e3a91aa0b5d5c4b9d
Yuichi Fujiki <fjk89025@Yuichis-MacBook-Air.local>
2011-10-21 10:21:57 -0500

    Initial Commit

---
+++ YFFontDisplayerTests/YFFontDisplayerTests.h
@@ -0,0 +1,13 @@
+//
+//  YFFontDisplayerTests.h
+//  YFFontDisplayerTests
+//
+//  Created by Yuichi Fujiki on 10/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface YFFontDisplayerTests : SenTestCase
+
+@end
---
+++ YFFontDisplayerTests/YFFontDisplayerTests.m
@@ -0,0 +1,32 @@
+//
+//  YFFontDisplayerTests.m
+//  YFFontDisplayerTests
+//
+//  Created by Yuichi Fujiki on 10/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "YFFontDisplayerTests.h"
+
+@implementation YFFontDisplayerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in YFFontDisplayerTests");
+}
+
+@end
